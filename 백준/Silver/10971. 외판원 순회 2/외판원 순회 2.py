cd022fcad64de06c9e9640183f6a99c52a322e48
HyungWook Kim (Luca) <141551860+Lucainr@users.noreply.github.com>
2025-07-15 17:50:21 +0900

    [Silver II] Title: 외판원 순회 2, Time: 692 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -0,0 +1,28 @@
+import sys
+from itertools import permutations
+
+n = int(sys.stdin.readline())
+W = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
+min_cost = float('inf')
+
+for perm in permutations(range(1, n)):
+    cost = 0
+    valid = True
+    current_route = (0,) + perm
+
+    for i in range(n - 1):
+        if W[current_route[i]][current_route[i + 1]] == 0:
+            valid = False
+            break
+        else:
+            cost += W[current_route[i]][current_route[i + 1]]
+        if cost >= min_cost:
+            valid = False
+            break
+
+    if valid and W[current_route[-1]][current_route[0]] != 0:
+        cost += W[current_route[-1]][current_route[0]]
+        min_cost = min(min_cost, cost)
+
+print(min_cost)
