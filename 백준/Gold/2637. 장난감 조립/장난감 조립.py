3fe5a81186e6d65b168147c698fd8070cac6c2ea
HyungWook Kim (Luca) <141551860+Lucainr@users.noreply.github.com>
2025-07-30 17:28:48 +0900

    [Gold II] Title: 장난감 조립, Time: 64 ms, Memory: 34976 KB -BaekjoonHub

---
+++ 백준/Gold/2637. 장난감 조립/장난감 조립.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+n = int(input())
+connect = [[] for _ in range(n + 1)]
+needs = [[0] * (n + 1) for _ in range(n + 1)]
+q = deque()
+degree = [0] * (n + 1)
+
+for _ in range(int(input())):
+    a, b, c = map(int, input().split())
+    connect[b].append((a, c))
+    degree[a] += 1
+
+for i in range(1, n + 1):
+    if degree[i] == 0:
+        q.append(i)
+
+while q:
+    now = q.popleft()
+    for next, next_need in connect[now]:
+        if needs[now].count(0) == n + 1:
+            needs[next][now] += next_need
+        else:
+            for i in range(1, n + 1):
+                needs[next][i] += needs[now][i] * next_need
+        degree[next] -= 1
+        if degree[next] == 0:
+            q.append(next)
+
+# ✅ index 1부터 출력 (0번 부품 제외)
+for idx, val in enumerate(needs[n]):
+    if idx > 0 and val > 0:
+        print(idx, val)
